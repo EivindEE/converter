646058ac2786d86c7a219107387f402016182544
EivindEE <eivind.elseth@gmail.com>
2013-01-24 13:52:42 +0100

    Changed the units into public constants in the Controller class

--- src/edu/uib/info231/controller/Controller.java
+++ src/edu/uib/info231/controller/Controller.java
@@ -12,7 +12,9 @@ import edu.uib.info231.model.Unit;
  */
 public class Controller {
 	private ConverterFrame frame;
-	
+	public static final Unit EUR = new CurrencyUnit("EUR", 7.442);
+	public static final Unit NOK = new CurrencyUnit("NOK", 1);
+	public static final Unit USD = new CurrencyUnit("USD", 5.588);
 	/**
 	 * Constructs an instance of Controller using a List<Unit> as an argument
 	 * @param currencies A List of currencies
@@ -31,9 +33,7 @@ public class Controller {
 	
 	public static void main(String[] args) {
 		List<Unit> units = new ArrayList<Unit>();
-		Unit[] unitArray = new Unit[]{new CurrencyUnit("EUR", 7.442),
-		                    new CurrencyUnit("NOK", 1),
-		                    new CurrencyUnit("USD", 5.588)};
+		Unit[] unitArray = new Unit[]{Controller.EUR, Controller.NOK, Controller.USD};
 		for (int i = 0; i < unitArray.length; i += 1 ) {
 			units.add(unitArray[i]);
 		}
