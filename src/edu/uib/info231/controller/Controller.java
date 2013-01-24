6ac51c5e121de8e79ddb8dc18012bccb9bbe3d02
EivindEE <eivind.elseth@gmail.com>
2013-01-24 13:33:27 +0100

    Added some constructors and JavaDoc

--- src/edu/uib/info231/controller/Controller.java
+++ src/edu/uib/info231/controller/Controller.java
@@ -3,20 +3,43 @@ package edu.uib.info231.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+
 import edu.uib.info231.gui.ConverterFrame;
 import edu.uib.info231.model.CurrencyUnit;
 import edu.uib.info231.model.Unit;
-
+/**
+ * The Controller class is responsible for setting up the Converter application
+ *
+ */
 public class Controller {
 	private ConverterFrame frame;
-
-
+	private ComboBoxModel comboBoxModel;
+	/**
+	 * Constructs an instance of Controller using a List<Unit> as an argument
+	 * @param currencies A List of currencies
+	 */
 	public Controller(List<Unit> currencies) {
-		super();
-		this.frame = new ConverterFrame(currencies);
+		this(currencies.toArray(new Unit[0]));
 	}
-
-
+	/**
+	 * Constructs an instance of Controller using an array of units (Unit[]) as an argument
+	 * @param currencies
+	 */
+	public Controller(Unit[] currencies) {
+		this(new DefaultComboBoxModel(currencies));
+	}
+	/**
+	 * Creates an instance of Controller using a ComboBoxModel as an argument
+	 * @param comboBoxModel
+	 */
+	public Controller(ComboBoxModel comboBoxModel) {
+		super();
+		this.comboBoxModel = comboBoxModel;
+		this.frame = new ConverterFrame(this.comboBoxModel);
+	}
+	
 	public static void main(String[] args) {
 		List<Unit> units = new ArrayList<Unit>();
 		Unit[] unitArray = new Unit[]{new CurrencyUnit("EUR", 7.442),
